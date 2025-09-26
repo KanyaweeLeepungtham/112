efad69f15efb747abb9a3f34625b9a65c7afca1c
Kanyawee Leepungtham <kanyawee6799@gmail.com>
2025-09-26 13:43:23 +0700

    Update 52.py

--- lab5/52.py
+++ lab5/52.py
@@ -1,10 +1,23 @@
 def strength(passwords):
     ans = []
     for p in passwords:
+        test1=0
+        test2=0
+        test3=0
         if p == "" or p.isalpha() or len(p) < 8:
             ans.append("weak")
-        elif len(p) >= 12:
-            ans.append("strong")
+        elif len(p)>= 12:
+            for i in p:
+                if i.isalpha():
+                    test1 = 1
+                if i.isdigit():
+                    test2 = 1
+                if not i.isalnum():
+                    test3 =1
+            if (test1 == 1 and test2 == 1 and test3 == 1):
+                ans.append("strong")
+        # and not p.isalnum() and p.upper() and p.lower():
+           
         else:
             ans.append("ok")
     return ans
@@ -19,4 +32,4 @@ print(strength(["abc12345", "Password1", "Hello2025"]))
 
 print(strength([""])) 
 
-print(strength(["onlyletters", "Mix123", "GoodOne2023!", "Ultra$trongP@ssw0rd2025"]))
+print(strength(["onlyletters", "Mix123", "GoodOne2023!", "Ultra$trongP@ssw0rd2025"])) 
